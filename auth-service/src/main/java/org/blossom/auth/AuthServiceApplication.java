52811bf0fb5beffb43f6763de5f1cc1a4c582420
BernardoFMF <bernardo_fragoso91@hotmail.com>
2023-06-18 22:46:13 +0100

    SETUP: Bootstrap api gateway

--- auth-service/src/main/java/org/blossom/auth/AuthServiceApplication.java
+++ auth-service/src/main/java/org/blossom/auth/AuthServiceApplication.java
@@ -1,4 +1,4 @@
-package org.blossom;
+package org.blossom.auth;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- discovery-server/src/main/java/org/blossom/discovery/DiscoveryServerApplication.java
+++ discovery-server/src/main/java/org/blossom/discovery/DiscoveryServerApplication.java
@@ -1,4 +1,4 @@
-package org.blossom;
+package org.blossom.discovery;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
