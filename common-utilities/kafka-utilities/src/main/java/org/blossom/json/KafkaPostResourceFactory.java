11642c948504f9894b3eb7a1bf10c6eb0aafdaa0
BernardoFMF <bernardo_fragoso91@hotmail.com>
2023-09-17 14:48:56 +0100

    BUGFIX: Fixed kafka post deserializer.

--- common-utilities/kafka-utilities/src/main/java/org/blossom/json/KafkaPostResourceFactory.java
+++ common-utilities/kafka-utilities/src/main/java/org/blossom/json/KafkaPostResourceFactory.java
@@ -3,14 +3,23 @@ package org.blossom.json;
 import com.fasterxml.jackson.databind.JsonNode;
 import org.blossom.model.KafkaPostResource;
 
+import java.util.ArrayList;
+
 public class KafkaPostResourceFactory implements KafkaResourceFactory<KafkaPostResource> {
     @Override
     public KafkaPostResource create(JsonNode json) {
         return KafkaPostResource.builder()
                 .id(json.get("id").asText())
                 .userId(json.get("userId").asInt())
-                .media(null)
+                .media(parseArray(json.get("media")))
                 .description(json.get("description").asText())
                 .build();
+    }
+
+    private String[] parseArray(JsonNode node) {
+        ArrayList<String> values = new ArrayList<>(node.size());
+        node.elements().forEachRemaining(x -> values.add(x.asText()));
+
+        return values.toArray(String[]::new);
     }
 }
