f1b752248b08d31c6ba317ce08894e2e7fd5d963
BernardoFMF <bernardo_fragoso91@hotmail.com>
2023-10-14 14:20:41 +0100

    FEATURE: Add websocket support; Add websocket broker relay; Add websocket message handling.

---
+++ message-service/src/main/java/org/blossom/message/client/AuthClient.java
@@ -0,0 +1,13 @@
+package org.blossom.message.client;
+
+import org.blossom.message.dto.LocalTokenDto;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@FeignClient("auth-service")
+public interface AuthClient {
+    @GetMapping("/api/v1/auth/validate")
+    ResponseEntity<LocalTokenDto> validate(@RequestParam("token") String token);
+}
---
+++ message-service/src/main/java/org/blossom/message/controller/WsMessageController.java
@@ -0,0 +1,66 @@
+package org.blossom.message.controller;
+
+import org.blossom.message.dto.DeleteMessageDto;
+import org.blossom.message.dto.PublishMessageDto;
+import org.blossom.message.dto.UpdateMessageDto;
+import org.blossom.message.entity.Message;
+import org.blossom.message.enums.BroadcastType;
+import org.blossom.message.exception.ChatNotFoundException;
+import org.blossom.message.exception.IllegalMessageOperationException;
+import org.blossom.message.exception.MessageNotFoundException;
+import org.blossom.message.exception.UserNotFoundException;
+import org.blossom.message.service.BroadcastService;
+import org.blossom.message.service.MessageService;
+import org.blossom.model.CommonUserDetails;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.messaging.handler.annotation.DestinationVariable;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.simp.SimpMessageHeaderAccessor;
+import org.springframework.security.authentication.AuthenticationCredentialsNotFoundException;
+import org.springframework.security.core.Authentication;
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class WsMessageController {
+    @Autowired
+    private BroadcastService broadcastService;
+
+    @Autowired
+    private MessageService messageService;
+
+    @MessageMapping("/chat/{chatId}/publishMessage")
+    public void handlePublishMessage(@DestinationVariable int chatId, PublishMessageDto publishMessage, SimpMessageHeaderAccessor headerAccessor) throws ChatNotFoundException, UserNotFoundException {
+        CommonUserDetails userDetails = ensureAuthentication(headerAccessor);
+
+        Message message = messageService.createMessage(publishMessage, chatId, userDetails.getUserId());
+
+        broadcastService.broadcastMessage(chatId, message, BroadcastType.MESSAGE_CREATED);
+    }
+
+    @MessageMapping("/chat/{chatId}/deleteMessage")
+    public void handleDeleteMessage(@DestinationVariable int chatId, DeleteMessageDto deleteMessage, SimpMessageHeaderAccessor headerAccessor) throws ChatNotFoundException, MessageNotFoundException, IllegalMessageOperationException {
+        CommonUserDetails userDetails = ensureAuthentication(headerAccessor);
+
+        Message message = messageService.deleteMessage(deleteMessage, userDetails.getUserId());
+
+        broadcastService.broadcastMessage(chatId, message, BroadcastType.MESSAGE_DELETED);
+    }
+
+    @MessageMapping("/chat/{chatId}/updateMessage")
+    public void handleUpdateMessage(@DestinationVariable int chatId, UpdateMessageDto updateMessage, SimpMessageHeaderAccessor headerAccessor) throws ChatNotFoundException, MessageNotFoundException, IllegalMessageOperationException {
+        CommonUserDetails userDetails = ensureAuthentication(headerAccessor);
+
+        Message message = messageService.updateMessage(updateMessage, userDetails.getUserId());
+
+        broadcastService.broadcastMessage(chatId, message, BroadcastType.MESSAGE_UPDATED);
+    }
+
+    private static CommonUserDetails ensureAuthentication(SimpMessageHeaderAccessor headerAccessor) {
+        Authentication authentication = (Authentication) headerAccessor.getUser();
+        if (authentication == null) {
+            throw new AuthenticationCredentialsNotFoundException("User not authenticated");
+        }
+
+        return (CommonUserDetails) authentication.getPrincipal();
+    }
+}
