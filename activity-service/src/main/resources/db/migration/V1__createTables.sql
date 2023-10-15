dc9c70715f4c773bf088ef7190905ba51e5d4685
BernardoFMF <bernardo_fragoso91@hotmail.com>
2023-10-15 15:43:45 +0100

    BUGFIX: Fixed db migration on activity service.

--- activity-service/src/main/resources/db/migration/V1__createTables.sql
+++ activity-service/src/main/resources/db/migration/V1__createTables.sql
@@ -1,6 +1,13 @@
+CREATE TABLE Blossom_Local_User (
+    user_id INT PRIMARY KEY,
+    username VARCHAR(255) UNIQUE,
+    full_name VARCHAR(255),
+    image_url VARCHAR(255)
+);
+
 create table Blossom_Interaction (
 	id SERIAL PRIMARY KEY,
-	user_id INT,
+	user_id INT REFERENCES Blossom_Local_User(id),
     post_id VARCHAR(255),
     interaction_type VARCHAR(255) NOT NULL,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
@@ -8,7 +15,7 @@ create table Blossom_Interaction (
 
 CREATE TABLE Blossom_Comment (
     id SERIAL PRIMARY KEY,
-    user_id INT,
+    user_id INT REFERENCES Blossom_Local_User(id),
     post_id VARCHAR(255),
     parent_comment_id INT REFERENCES Blossom_Comment(id),
     top_level_comment_id INT REFERENCES Blossom_Comment(id),
@@ -16,11 +23,4 @@ CREATE TABLE Blossom_Comment (
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     updated_at TIMESTAMP,
     is_deleted BOOL DEFAULT FALSE
-);
-
-CREATE TABLE Blossom_Local_User (
-    user_id INT PRIMARY KEY,
-    username VARCHAR(255) UNIQUE,
-    full_name VARCHAR(255),
-    image_url VARCHAR(255)
 );
