7efc80cc2977c3809fccb6ee3142a97ae2f88815
BernardoFMF <bernardo_fragoso91@hotmail.com>
2023-10-16 09:18:20 +0100

    BUGFIX: Add kafka handler for social follow in message service.

--- message-service/src/main/java/org/blossom/message/kafka/inbound/KafkaResourceListener.java
+++ message-service/src/main/java/org/blossom/message/kafka/inbound/KafkaResourceListener.java
@@ -16,6 +16,9 @@ public class KafkaResourceListener implements KafkaConsumerFacade {
     @Autowired
     private UserResourceHandler userResourceHandler;
 
+    @Autowired
+    private SocialFollowResourceHandler socialFollowResourceHandler;
+
     Map<EventType, Map<ResourceType, Consumer<KafkaResource>>> handlers = new HashMap<>();
 
     @PostConstruct
@@ -28,12 +31,16 @@ public class KafkaResourceListener implements KafkaConsumerFacade {
         updateHandlers.put(ResourceType.USER, (event) -> userResourceHandler.update((KafkaUserResource) event));
         deleteHandlers.put(ResourceType.USER, (event) -> userResourceHandler.delete((KafkaUserResource) event));
 
+        creationHandlers.put(ResourceType.SOCIAL_FOLLOW, (event) -> socialFollowResourceHandler.save((KafkaSocialFollowResource) event));
+        updateHandlers.put(ResourceType.SOCIAL_FOLLOW, (event) -> socialFollowResourceHandler.update((KafkaSocialFollowResource) event));
+        deleteHandlers.put(ResourceType.SOCIAL_FOLLOW, (event) -> socialFollowResourceHandler.delete((KafkaSocialFollowResource) event));
+
         handlers.put(EventType.CREATE, creationHandlers);
         handlers.put(EventType.UPDATE, updateHandlers);
         handlers.put(EventType.DELETE, deleteHandlers);
     }
 
-    @KafkaListener(topics = "user-resource-event-message")
+    @KafkaListener(topics = {"user-resource-event-message", "social-follow-resource-event-message"})
     public void handleResource(ResourceEvent resourceEvent) {
         handlers.get(resourceEvent.getEventType()).get(resourceEvent.getResourceType()).accept(resourceEvent.getResource());
     }
