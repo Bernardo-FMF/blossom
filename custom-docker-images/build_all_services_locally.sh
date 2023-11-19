f34c6671df40b364e6a90632c293158fe6ebf086
BernardoFMF <bernardo_fragoso91@hotmail.com>
2023-11-19 17:11:24 +0000

    IMPROVE: Add script to build images locally; Add services to docker compose.

---
+++ custom-docker-images/build_all_services_locally.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+cd ..
+
+for service in activity-service api-gateway auth-service discovery-server feed-service image-service message-service notification-service post-service social-graph-service; do
+  echo "building docker image for $service"
+  docker build -t bernardofmf/blossom-$service:latest -f ./$service/Dockerfile ./$service/
+done
