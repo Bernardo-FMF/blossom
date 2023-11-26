0e314bfb1c87713dbcdf01579d7a6b2ef9c86109
BernardoFMF <bernardo_fragoso91@hotmail.com>
2023-11-26 11:46:13 +0000

    IMPROVE: Added env variables to all services to make them more generic.

--- custom-docker-images/build_all_services_locally.sh
+++ custom-docker-images/build_all_services_locally.sh
@@ -2,6 +2,8 @@
 
 cd ..
 
+mvn clean install
+
 for service in activity-service api-gateway auth-service discovery-server feed-service image-service message-service notification-service post-service social-graph-service; do
   echo "building docker image for $service"
   docker build -t bernardofmf/blossom-$service:latest -f ./$service/Dockerfile ./$service/
