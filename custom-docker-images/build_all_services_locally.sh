ee6ccb1ec7b140ed2663c5816fe69ba87a34a586
BernardoFMF <bernardo_fragoso91@hotmail.com>
2023-12-16 23:49:23 +0000

    IMPROVE: Add flag to docker build script.

--- custom-docker-images/build_all_services_locally.sh
+++ custom-docker-images/build_all_services_locally.sh
@@ -1,8 +1,30 @@
 #!/bin/bash
 
+display_usage() {
+  echo "Usage: $0 [-b]"
+  echo "Options:"
+  echo "  -b  Perform 'mvn clean install' before building docker images"
+}
+
+if [ "$1" == "-h" ] || [ "$1" == "--help" ]; then
+  display_usage
+  exit 1
+fi
+
+perform_maven=false
+
+if [ "$1" == "-b" ]; then
+  perform_maven=true
+fi
+
 cd ..
 
-mvn clean install
+if [ "$perform_maven" = true ]; then
+  echo "Performing 'mvn clean install'..."
+  mvn clean install
+else
+  echo "Building Docker images without 'mvn clean install'..."
+fi
 
 for service in activity-service api-gateway auth-service discovery-server feed-service image-service message-service notification-service post-service social-graph-service; do
   echo "building docker image for $service"
