25113988f935a9e32e7e8228d8d538c9c2bd040b
BernardoFMF <bernardo_fragoso91@hotmail.com>
2024-01-09 21:35:31 +0000

    REFACTOR: Removed duplicate logic when sending emails.

--- auth-service/src/main/java/org/blossom/auth/email/EmailService.java
+++ auth-service/src/main/java/org/blossom/auth/email/EmailService.java
@@ -1,6 +1,8 @@
 package org.blossom.auth.email;
 
+import jakarta.mail.MessagingException;
 import jakarta.mail.internet.MimeMessage;
+import lombok.extern.log4j.Log4j2;
 import org.blossom.auth.dto.UserDto;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
@@ -13,6 +15,7 @@ import java.util.HashMap;
 import java.util.Map;
 
 @Service
+@Log4j2
 public class EmailService {
     @Autowired
     JavaMailSender mailSender;
@@ -28,33 +31,41 @@ public class EmailService {
 
     public void sendPasswordRecoveryEmail(UserDto userDto) {
         try {
-            MimeMessage message = mailSender.createMimeMessage();
-            MimeMessageHelper helper = new MimeMessageHelper(
-                    message, MimeMessageHelper.MULTIPART_MODE_MIXED_RELATED,
-                    StandardCharsets.UTF_8.name()
-            );
-
-            helper.setTo(userDto.getEmail());
-            helper.setFrom(email);
-
-            Map<String, Object> variables = new HashMap<>();
-            variables.put("username", userDto.getUsername());
-            variables.put("expiration-date", userDto.getExpirationDate());
-            variables.put("callbackUrl", buildCallbackUrl(userDto.getToken(), userDto.getId()));
-
-            helper.setText(thymeleafService.createContent("password-recovery.html", variables), true);
-            helper.setSubject("Blossom: Password Recovery");
-            mailSender.send(message);
-        } catch (Exception e) {
-            e.printStackTrace();
+            sendEmail(userDto, "password-recovery.html", "Blossom: Password Recovery");
+        } catch (Exception ex) {
+            log.error("Error sending recovery email to user " + userDto.getId(), ex);
         }
     }
 
-    public void sendVerificationEmail(UserDto dto) {
-
+    public void sendVerificationEmail(UserDto userDto) {
+        try {
+            sendEmail(userDto, "email-verification.html", "Blossom: Email Verification");
+        } catch (Exception ex) {
+            log.error("Error sending verification email to user " + userDto.getId(), ex);
+        }
     }
 
-    private Object buildCallbackUrl(String token, int id) {
+    private void sendEmail(UserDto userDto, String template, String subject) throws MessagingException {
+        MimeMessage message = mailSender.createMimeMessage();
+        MimeMessageHelper helper = new MimeMessageHelper(
+                message, MimeMessageHelper.MULTIPART_MODE_MIXED_RELATED,
+                StandardCharsets.UTF_8.name()
+        );
+
+        helper.setTo(userDto.getEmail());
+        helper.setFrom(email);
+
+        Map<String, Object> variables = new HashMap<>();
+        variables.put("username", userDto.getUsername());
+        variables.put("expiration-date", userDto.getExpirationDate());
+        variables.put("callbackUrl", buildCallbackUrl(userDto.getToken(), userDto.getId()));
+
+        helper.setText(thymeleafService.createContent(template, variables), true);
+        helper.setSubject(subject);
+        mailSender.send(message);
+    }
+
+    private String buildCallbackUrl(String token, int id) {
         return String.format(callbackUrl + "?token=%s&userId=%s", token, id);
     }
 }
