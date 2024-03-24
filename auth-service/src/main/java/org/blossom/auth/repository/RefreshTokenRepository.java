773561cc2c1ac3b634c8f3b4e2c14e19334f910c
BernardoFMF <bernardo_fragoso91@hotmail.com>
2024-03-24 15:43:00 +0000

    BUGFIX: Fixed refresh token database accesses.

--- auth-service/src/main/java/org/blossom/auth/repository/RefreshTokenRepository.java
+++ auth-service/src/main/java/org/blossom/auth/repository/RefreshTokenRepository.java
@@ -1,5 +1,6 @@
 package org.blossom.auth.repository;
 
+import jakarta.transaction.Transactional;
 import org.blossom.auth.entity.RefreshToken;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
@@ -10,11 +11,14 @@ import java.time.Instant;
 import java.util.Optional;
 
 public interface RefreshTokenRepository extends JpaRepository<RefreshToken, Integer> {
-    void deleteByUserId(Integer id);
+    @Modifying
+    @Query("delete from RefreshToken rt where rt.user.id = :id")
+    void deleteByUserId(@Param("id") Integer id);
 
     Optional<RefreshToken> findByTokenAndUserId(String token, Integer userId);
 
+    @Transactional
     @Modifying
-    @Query("delete from RefreshToken rt where rt.expirationDate <= :now")
-    void deleteAllExpiredSince(@Param("now") Instant now);
+    @Query("delete from RefreshToken rt where rt.expirationDate <= :date")
+    void deleteAllExpiredSince(@Param("date") Instant date);
 }
