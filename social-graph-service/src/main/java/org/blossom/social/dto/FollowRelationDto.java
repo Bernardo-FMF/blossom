1b01138e89f078ffc716e0584169691547ef582f
BernardoFMF <bernardo_fragoso91@hotmail.com>
2024-04-28 19:50:53 +0100

    IMPROVE: Added attribute to validate if the user is himself.

--- social-graph-service/src/main/java/org/blossom/social/dto/FollowRelationDto.java
+++ social-graph-service/src/main/java/org/blossom/social/dto/FollowRelationDto.java
@@ -1,9 +1,12 @@
 package org.blossom.social.dto;
 
 import lombok.Builder;
+import lombok.Getter;
 
+@Getter
 @Builder
 public class FollowRelationDto {
+    boolean isSelf;
     boolean mutualFollow;
     boolean userFollows;
     boolean userIsFollowed;
--- social-graph-service/src/main/java/org/blossom/social/mapper/impl/FollowRelationDtoMapper.java
+++ social-graph-service/src/main/java/org/blossom/social/mapper/impl/FollowRelationDtoMapper.java
@@ -5,11 +5,12 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class FollowRelationDtoMapper {
-    public FollowRelationDto toDto(boolean userFollows, boolean userIsFollowed) {
+    public FollowRelationDto toDto(boolean userFollows, boolean userIsFollowed, boolean isSelf) {
         return FollowRelationDto.builder()
                 .userIsFollowed(userIsFollowed)
                 .userFollows(userFollows)
                 .mutualFollow(userFollows && userIsFollowed)
+                .isSelf(isSelf)
                 .build();
     }
 }
